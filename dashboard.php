91b7cd81f311219108e83079b276438ea70b44f0
cliffamadeus <dev.clfm5t12@gmail.com>
2024-06-24 09:51:26 +0800

    Added Datatable template

--- dashboard.php
+++ dashboard.php
@@ -1,33 +1,43 @@
 <?php
 // Initialize the session
 session_start();
- 
+
 // Check if the user is logged in, if not then redirect him to login page
-if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
-    header("location: login.php");
-    exit;
+if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
+  header("location: login.php");
+  exit;
 }
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <title>Dashboard</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-    <style>
-        table tr td:last-child{
-            width: 120px;
-        }
-    </style>
-    <script>
-        $(document).ready(function(){
-            $('[data-toggle="tooltip"]').tooltip();   
-        });
-    </script>
+  <meta charset="UTF-8">
+  <title>Dashboard</title>
+  <!--Legacy Scripts-->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+  <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+
+  <link rel="stylesheet" href="">
+
+
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://cdn.datatables.net/2.0.8/css/dataTables.bootstrap5.css">
+
+
+
+
+  <style>
+    table tr td:last-child {
+      width: 120px;
+    }
+  </style>
+  <script>
+    $(document).ready(function() {
+      $('[data-toggle="tooltip"]').tooltip();
+    });
+  </script>
 </head>
 
 <!-- Navigation Bar -->
@@ -63,8 +73,8 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
             </ul>
           </li>
           <p>
-             
-             
+
+
           </p>
         </ul>
         <form class="d-flex mt-3" role="search">
@@ -77,61 +87,80 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
 </nav>
 
 <body>
-<div class="container-fluid">
-  <br>
+
+  <div class="container-fluid">
+    <br>
     <h1 class="my-5">Hi, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>. Welcome to our site.</h1>
-      <div class="col">
-        <div class="mt-5 mb-3 clearfix">
-          <h2 class="pull-left">Alumni Details</h2>
-          <a href="create.php" class="btn btn-success pull-right"><i class="fa fa-plus"></i> Add New Alumni</a>
-        </div>
-        <?php
-          // Include config file
-          require_once "./db/config.php";
-                      
-          // Attempt select query execution
-          $sql = "SELECT * FROM employees";
-          if($result = $pdo->query($sql)){
-            if($result->rowCount() > 0){
-              echo '<table class="table table-bordered table-striped">';
-                echo "<thead>";
-                  echo "<tr>";
-                    echo "<th>#</th>";
-                    echo "<th>Name</th>";
-                    echo "<th>Address</th>";
-                    echo "<th>Salary</th>";
-                    echo "<th>Action</th>";
-                  echo "</tr>";
-                echo "</thead>";
-                    echo "<tbody>";
-                      while($row = $result->fetch()){
-                        echo "<tr>";
-                        echo "<td>" . $row['id'] . "</td>";
-                        echo "<td>" . $row['name'] . "</td>";
-                        echo "<td>" . $row['address'] . "</td>";
-                        echo "<td>" . $row['salary'] . "</td>";
-                        echo "<td>";
-                        echo '<a href="read.php?id='. $row['id'] .'" class="mr-3" title="View Record" data-toggle="tooltip"><span class="fa fa-eye"></span></a>';
-                        echo '<a href="update.php?id='. $row['id'] .'" class="mr-3" title="Update Record" data-toggle="tooltip"><span class="fa fa-pencil"></span></a>';
-                        echo '<a href="delete.php?id='. $row['id'] .'" title="Delete Record" data-toggle="tooltip"><span class="fa fa-trash"></span></a>';
-                        echo "</td>";
-                        echo "</tr>";
-                      }
-                    echo "</tbody>";                            
-              echo "</table>";
-              // Free result set
-              unset($result);
-            } else{
-              echo '<div class="alert alert-danger"><em>No records were found.</em></div>';
-            }
-            } else{
-              echo "Oops! Something went wrong. Please try again later.";
-            }
-                      
-            // Close connection
-            unset($pdo);
-        ?>
-      </div>                  
-</div>
+    <div class="col">
+      <div class="mt-5 mb-3 clearfix">
+        <h2 class="pull-left">Alumni Details</h2>
+        <a href="create.php" class="btn btn-success pull-right"><i class="fa fa-plus"></i> Add New Alumni</a>
+      </div>
+      <?php
+      // Include config file
+      require_once "./db/config.php";
+
+      // Attempt select query execution
+      $sql = "SELECT * FROM employees";
+      if ($result = $pdo->query($sql)) {
+        if ($result->rowCount() > 0) {
+      ?>
+          <table id="example" class="table table-striped" style="width:100%;margin-top:5rem;">
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Name</th>
+                <th>Address</th>
+                <th>Salary</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              <?php while ($row = $result->fetch()) : ?>
+                <tr>
+                  <td><?= $row['id'] ?></td>
+                  <td><?= $row['name'] ?></td>
+                  <td><?= $row['address'] ?></td>
+                  <td><?= $row['salary'] ?></td>
+                  <td>
+                    <a href="read.php?id=<?= $row['id'] ?>" class="mr-3" title="View Record" data-toggle="tooltip"><span class="fa fa-eye"></span></a>
+                    <a href="update.php?id=<?= $row['id'] ?>" class="mr-3" title="Update Record" data-toggle="tooltip"><span class="fa fa-pencil"></span></a>
+                    <a href="delete.php?id=<?= $row['id'] ?>" title="Delete Record" data-toggle="tooltip"><span class="fa fa-trash"></span></a>
+                  </td>
+                </tr>
+              <?php endwhile; ?>
+            </tbody>
+            <tfoot>
+              <tr>
+                <th>#</th>
+                <th>Name</th>
+                <th>Address</th>
+                <th>Salary</th>
+                <th>Action</th>
+              </tr>
+            </tfoot>
+          </table>
+      <?php
+          // Free result set
+          unset($result);
+        } else {
+          echo '<div class="alert alert-danger"><em>No records were found.</em></div>';
+        }
+      } else {
+        echo "Oops! Something went wrong. Please try again later.";
+      }
+
+      // Close connection
+      unset($pdo);
+      ?>
+    </div>
+  </div>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/js/bootstrap.bundle.min.js"></script>
+  <script src="https://cdn.datatables.net/2.0.8/js/dataTables.js"></script>
+  <script src="https://cdn.datatables.net/2.0.8/js/dataTables.bootstrap5.js"></script>
+  <script>
+    new DataTable('#example');
+  </script>
 </body>
+
 </html>
